2dde3d9e3c64c5a3a895340a2b9dae63556c09bf
AngusLaw612 <claw624@canon-slade.bolton.sch.uk>
2024-12-09 10:03:44 +0000

    09/12/2024

--- Self Exercise/Simple Calculator(Finish)/script.js
+++ Self Exercise/Simple Calculator(Finish)/script.js
@@ -23,75 +23,89 @@ Object.keys(buttonMappings).forEach(id => {
     document.getElementById(id).addEventListener('click', buttonMappings[id]);
 });
 
+// Function to update the display with the given value
 function updateDisplay(value) {
     display.innerText += value;
 }
 
+// Functions for each button to update the display with the corresponding value
+function buttonInput7() { updateDisplay(7); }
+function buttonInput8() { updateDisplay(8); }
+function buttonInput9() { updateDisplay(9); }
+function buttonInputdash() { updateDisplay('/'); }
+function buttonInput4() { updateDisplay(4); }
+function buttonInput5() { updateDisplay(5); }
+function buttonInput6() { updateDisplay(6); }
+function buttonInputstar() { updateDisplay('*'); }
+function buttonInput1() { updateDisplay(1); }
+function buttonInput2() { updateDisplay(2); }
+function buttonInput3() { updateDisplay(3); }
+function buttonInputminus() { updateDisplay('-'); }
+function buttonInput0() { updateDisplay(0); }
 
-function buttonInput7() {
-    updateDisplay(7);
-}
-
-function buttonInput8() {
-    updateDisplay(8);
-}
-
-function buttonInput9() {
-    updateDisplay(9);
-}
-
-function buttonInputdash() {
-    updateDisplay('/');
-}
-
-function buttonInput4() {
-    updateDisplay(4);
-}
-
-function buttonInput5() {
-    updateDisplay(5);
-}
-
-function buttonInput6() {
-    updateDisplay(6);
-}
-
-function buttonInputstar() {
-    updateDisplay('*');
-}
-
-function buttonInput1() {
-    updateDisplay(1);
-}
-
-function buttonInput2() {
-    updateDisplay(2);
-}
-
-function buttonInput3() {
-    updateDisplay(3);
-}
-
-function buttonInputminus() {
-    updateDisplay('-');
-}
-
-function buttonInput0() {
-    updateDisplay(0);
-}
-
+// Function to clear the display
 function buttonInputC() {
     display.innerText = '';
 }
 
+// Function to evaluate the expression and display the result without using eval
 function buttonInputequal() {
     try {
-        display.innerText = eval(display.innerText);
+        const result = evaluateExpression(display.innerText);
+        display.innerText = result;
     } catch {
         display.innerText = 'Error';
     }
 }
 
-function buttonInputadd() {
-    updateDisplay('+');
+// Function to update the display with a plus sign
+function buttonInputadd() { updateDisplay('+'); }
+
+// Function to evaluate a mathematical expression
+function evaluateExpression(expression) {
+    const tokens = expression.match(/(\d+|\+|\-|\*|\/)/g);
+    if (!tokens) throw new Error('Invalid expression');
+
+    let stack = [];
+    let currentNumber = '';
+    let currentOperator = null;
+
+    tokens.forEach(token => {
+        if (/\d/.test(token)) {
+            currentNumber += token;
+        } else {
+            if (currentNumber) {
+                stack.push(parseFloat(currentNumber));
+                currentNumber = '';
+            }
+            if (currentOperator) {
+                const b = stack.pop();
+                const a = stack.pop();
+                stack.push(applyOperator(a, b, currentOperator));
+            }
+            currentOperator = token;
+        }
+    });
+
+    if (currentNumber) {
+        stack.push(parseFloat(currentNumber));
+    }
+    if (currentOperator) {
+        const b = stack.pop();
+        const a = stack.pop();
+        stack.push(applyOperator(a, b, currentOperator));
+    }
+
+    return stack[0];
+}
+
+// Function to apply an operator to two numbers
+function applyOperator(a, b, operator) {
+    switch (operator) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;
+        default: throw new Error('Unknown operator');
+    }
 }
