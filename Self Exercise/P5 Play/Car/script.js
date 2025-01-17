02171e6ed23e3037f692eeeb9137b3e926c29beb
AngusLaw612 <claw624@canon-slade.bolton.sch.uk>
2025-01-17 12:59:51 +0000

    17/01/2025

--- Self Exercise/P5 Play/Car/script.js
+++ Self Exercise/P5 Play/Car/script.js
@@ -1,9 +1,19 @@
-let road , ball;
+let road , ball, ground ;
+
+async function pauseLoop() {
+  let waitTime = 100000;
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+    await new Promise(resolve => setTimeout(resolve, waitTime)); // Pause for 1 second
+    waitTime = waitTime - 10
+  }
+}
+ 
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 
-    world.gravity.y = 9.81;
+    world.gravity.y = 9;
 
     road = new Sprite();
     road.h = 10;
@@ -13,11 +23,22 @@ function setup() {
     road.color = "pink";
     road.stroke = "red";
     road.collider = 'static';
+    
 
     ball = new Sprite();
     ball.d = 50;
     ball.x = 450;
-    ball.y = 60;;
+    ball.y = 60;
+    ball.drag = 1
+
+   let grade = [[400, 0]];
+    for (let i = 0; i < 100; i++) {
+      grade.push([random(50, 200), random(-30, 30)]);
+    }
+    ground = new Sprite(450, 450, grade, 's');
+    
+   
+    
 
 
 }
@@ -25,24 +46,35 @@ function setup() {
 function update() {
 	clear();
 
-	ball.speed = 3;
-	
-	if (kb.pressing('w')) {
-		ball.direction = -90;
-	} else if (kb.pressing('s')) {
-		ball.direction = 90;
-	} else if (kb.pressing('a')) {
-		ball.direction = 180;
-	} else if (kb.pressing('d')) {
-		ball.direction = 0;
-	} else {
-	  ball.speed = 0;
-	}
 }
 
 function draw() {
     clear();
+   
+    camera.x = ball.x 
+    camera.y = ball.y 
+
+
+    if (ball.colliding(ground))	{
+      if (kb.presses(' ')) {
+        ball.vel.y = 20;
+      }
+    }
+
+    if (kb.pressing('d')) {
+      j = 0
+      ball.vel.x = 0
+      for (let j = 0;  j < 100;  j = j + 0.05){
+        pauseLoop();
+        ball.vel.x = 3 + j;
+        
+    }
+   
+    }else if (kb.pressing('a')) {
+      ball.vel.x = -3;
+    }
+  }
+
   
-    road.draw();
-    ball.draw();
-}
+  
+
